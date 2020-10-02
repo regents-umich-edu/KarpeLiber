7ff548d41de99febc1b63e23d11e3ac1b4e513e7
Mr. Lance E Sloan (sloanlance) <sloanlance@users.noreply.github.com>
2020-10-01 22:00:30 -0400

    add "main" app to project, basic view/model

--- KarpeLiber/urls.py
+++ KarpeLiber/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+import main
 
 urlpatterns = [
+    path(f'{main.__name__}/', include(main.urls)),
     path('admin/', admin.site.urls),
 ]
