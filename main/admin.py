61796fb20e171b3f8e1ff72f63a0e77f88d5328c
Mr. Lance E Sloan (sloanlance) <sloanlance@users.noreply.github.com>
2020-10-02 23:15:05 -0400

    add models to admin UI

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+# register all models to appear in admin UI
+# TODO: add a flag to models to indicate whether they should be shown/hidden
+for cls in models.__dict__.values():
+    if isinstance(cls, type):
+        admin.site.register(cls)
