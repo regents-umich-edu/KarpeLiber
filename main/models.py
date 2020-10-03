8dbc886ef038fa1246363399b08dd0c701231ebe
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2020-10-02 23:15:58 -0400

    added columns; more like old DB

--- main/models.py
+++ main/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+
 class Volume(models.Model):
-    name = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    class Meta:
+        db_table = 'volume'
 
+    title = models.CharField(max_length=200)
+    dateBegin = models.DateField('beginning date')
+    dateEnd = models.DateField('ending date')
+    pages = models.BigIntegerField('number of pages')
+    available = models.BooleanField('available online', default=False)
 
+    def __str__(self):
+        return self.title
