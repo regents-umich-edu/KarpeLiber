e607b2438e1ffb50c36a59dca704ed67880d7aa7
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2020-10-08 09:46:28 -0400

    improved automatic model registration
    This will probably be removed later, because model-specific classes will be needed to get the features into the admin interface we want.

--- main/admin.py
+++ main/admin.py
@@ -4,6 +4,5 @@ from main import models
 
 # register all models to appear in admin UI
 # TODO: add a flag to models to indicate whether they should be shown/hidden
-for cls in models.__dict__.values():
-    if isinstance(cls, type):
-        admin.site.register(cls)
+admin.site.register(
+    [cls for cls in models.__dict__.values() if isinstance(cls, type)])
