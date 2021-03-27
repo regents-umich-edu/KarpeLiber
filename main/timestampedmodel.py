55b292a8ac1659024bf3f351dbccbf87c760f7d9
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2021-03-26 23:38:46 -0400

    allow null timestamps
    Old data from production doesn't always include timestamps for topic and item added and updated times, so allow nulls.

--- main/timestampedmodel.py
+++ main/timestampedmodel.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class TimeStampedModel(models.Model):
-    addedTime = models.DateTimeField(auto_now_add=True)
-    updatedTime = models.DateTimeField(auto_now=True)
+    # null allowed to accommodate import of historical data without values
+    addedTime = models.DateTimeField(auto_now_add=True, null=True)
+    updatedTime = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         abstract = True
