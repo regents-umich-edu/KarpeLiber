75ff02d50fcfaa3267cc09a5d85d1c79749efd3b
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2021-04-23 16:12:10 -0400

    logic to handle result page requests

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Tuple
+from typing import Tuple, Optional
 
 from django.db.models import QuerySet, Count
 from django.http import HttpResponse, HttpRequest
@@ -19,17 +19,21 @@ def search(request: HttpRequest):
     # whitespace: strip leading/trailing & reduce embedded
     searchString: str = ' '.join(request.GET.get('searchString', '').split())
 
-    topicId: int = safeInt(
+    topicId: Optional[int] = safeInt(
         request.GET.get('phraseId', request.GET.get('topicId')))
-    moreTopics: int = safeInt(
+    moreTopics: Optional[int] = safeInt(
         request.GET.get('morePhrases', request.GET.get('moreTopics')))
-    moreItems: int = safeInt(request.GET.get('moreItems'))
+    moreItems: Optional[int] = safeInt(request.GET.get('moreItems'))
 
     topic: QuerySet = None
     topics: QuerySet = None
     itemPages: QuerySet = None
 
+    maxTopics = 25 if moreTopics is not None else 15
+    maxItems = 25 if moreItems is not None else 10
+
     itemPagesOrderFields: Tuple[str, ...] = ('-year', 'page', 'item__name')
+    itemPagesFilterArgs: Optional[dict[str, str]] = None
 
     if searchString or topicId:
         if topicId:
@@ -37,27 +41,55 @@ def search(request: HttpRequest):
             logger.debug(topic)
 
             itemPagesFilterArgs = {'item__topic': topic}
-        else:
+        elif not moreItems:
             topics = (Topic.objects.filter(name__icontains=searchString)
                       .annotate(itemCount=Count('items__itemPages'))
                       .order_by('name'))
             logger.debug(topics)
 
+        if itemPagesFilterArgs is None:
             itemPagesFilterArgs = {'item__name__icontains': searchString}
 
-        itemPages = (ItemPage.objects.filter(**itemPagesFilterArgs)
-                     .order_by(*itemPagesOrderFields))
-        logger.debug(itemPages)
+        if not moreTopics:
+            itemPages = (ItemPage.objects.filter(**itemPagesFilterArgs)
+                         .order_by(*itemPagesOrderFields))
+            logger.debug(itemPages)
+
+        if moreTopics:
+            if moreTopics == len(topics):
+                moreTopics -= maxTopics
+            if moreTopics < 0:
+                moreTopics = 0
+            elif moreTopics > len(topics):
+                moreTopics = len(topics) - (len(topics) % maxTopics)
+            if moreTopics == len(topics):
+                moreTopics -= maxTopics
+
+        if moreItems:
+            if moreItems == len(itemPages):
+                moreItems -= maxItems
+            if moreItems < 0:
+                moreItems = 0
+            elif moreItems > len(itemPages):
+                moreItems = len(itemPages) - (len(itemPages) % maxItems)
+            if moreItems == len(itemPages):
+                moreItems -= maxItems
+
+        topicIndex: int = 0 if not moreTopics else moreTopics
+        itemIndex: int = 0 if not moreItems else moreItems
 
         return render(request, 'main/search.html', {
             'searchString': searchString,
             'topic': topic,
-            'topics': topics[0:15] if topics else None,
+            'topics':
+                topics[topicIndex:topicIndex + maxTopics] if topics else None,
             'topicsTotalCount': topics.count() if topics else None,
-            'itemPages': itemPages[0:10] if itemPages else None,
+            'itemPages': itemPages[itemIndex:itemIndex + maxItems] if itemPages else None,
             'itemPagesTotalCount': itemPages.count() if itemPages else None,
             'moreTopics': moreTopics,
             'moreItems': moreItems,
+            'maxTopics': maxTopics,
+            'maxItems': maxItems,
         })
     else:
         return render(request, 'main/search.html')
