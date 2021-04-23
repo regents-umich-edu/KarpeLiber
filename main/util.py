da93df296ee3ed4094fce567ecf67b5adc5290f2
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2021-04-23 15:58:21 -0400

    reformat and add typing

--- main/util.py
+++ main/util.py
@@ -1,10 +1,16 @@
-def safeInt(x, base=10):
+from typing import Optional, Any
+
+
+def safeInt(x: Any, base: int = 10) -> Optional[int]:
     '''
     Make an integer from object `x`; return `None` on failure.
     '''
+
+    result: Optional[int]
 
     try:
         result = int(x, base)
     except (ValueError, TypeError):
         result = None
+
     return result
